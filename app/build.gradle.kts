13dd0bfbb20a8986f2cdf76314ffec014fccb71c
kku <ruddns5315@gmail.com>
2024-04-29 23:19:18 +0900

    카카오 로그인 UI 생성 카카오 모듈 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,12 +43,20 @@ android {
 
 dependencies {
 
-    //구글로그인
+    //구글 로그인
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
 
+    //카카오 로그인
+    implementation ("com.kakao.sdk:v2-all:2.20.1") // 전체 모듈 설치, 2.11.0 버전부터 지원
+    implementation ("com.kakao.sdk:v2-user:2.20.1") // 카카오 로그인 API 모듈
+    implementation ("com.kakao.sdk:v2-share:2.20.1") // 카카오톡 공유 API 모듈
+    implementation ("com.kakao.sdk:v2-talk:2.20.1") // 카카오톡 채널, 카카오톡 소셜, 카카오톡 메시지 API 모듈
+    implementation ("com.kakao.sdk:v2-friend:2.20.1") // 피커 API 모듈
+    implementation ("com.kakao.sdk:v2-navi:2.20.1") // 카카오내비 API 모듈
+    implementation ("com.kakao.sdk:v2-cert:2.20.1") // 카카오톡 인증 서비스 API 모듈
 
 
 
